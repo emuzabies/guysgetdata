8f9cc0e63e618a0fbfaba0dfd5f0eefb73404b87
emuzabies <emuzabies@gmail.com>
2021-03-25 18:13:27 +0700

    emu479-push-guys-getdata

---
+++ backend/Guysdata/Guysdata/Controllers/UserinfoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Guysdata.Model;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Guysdata.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserinfoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public UserinfoController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //เพิ่มข้อมูลจากDatabaseสำหรับ api/userinfo
+        [HttpPost]
+        public JsonResult Post(Userinfo userinfo)
+        {
+            //query สำหรับสร้างหรือเก็บข้อมูลจากDatabaseสำหรับ api/userinfo
+            //emu479p1 :guysplatformapi/userinfo/post
+            string query = @"
+                    insert into dbo.UserInfo(ID, Firstname, Lastname, Birthdate, Tel, Email) values
+                    ('" + userinfo.ID + @"',
+                     '" + userinfo.Firstname + @"',
+                     '" + userinfo.Lastname + @"',
+                     '" + userinfo.Birthdate + @"',
+                     '" + userinfo.Tel + @"',
+                     '" + userinfo.Email + @"')
+                       ";
+            DataTable table = new DataTable();
+            //เชื่อมต่อกับฐานข้อมูล
+            //emu479p1 :guysplatformapi/userinfo/post
+            string sqlDataSource = "Server=tcp:guysdevops.database.windows.net,1433;Initial Catalog=GuysDevOps;Persist Security Info=False;User ID=guysdevops;Password=guys.1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
---
+++ backend/Guysdata/Guysdata/Model/Userinfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guysdata.Model
+{
+    public class Userinfo
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+    }
+}
